443f16075502acddf0e0799ea28109d721b15429
sajasmohammed <sajasam97@gmail.com>
2021-09-25 10:11:42 -0700

    add layout for home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -11,18 +11,49 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(defaulPadding),
-          child: Column(
-            children: [],
+        child: Column(
+            children: [
+              Expanded(
+                flex: 6,
+                child: Container(
+                  width: double.infinity,
+                  color: Colors.lightBlue[200],
+                  child: Padding(
+                    padding: const EdgeInsets.all(defaulPadding),
+                    child: Column(
+                      children: [
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: double.infinity,
+                  color: cblack,
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Text("Edit Profile", textAlign: TextAlign.center, style: TextStyle(color: cwhite, fontWeight: FontWeight.bold),),
+                  )
+                ),
+              ),
+              Expanded(
+                flex: 12,
+                child:Container(
+                  color: Colors.grey[200],
+                ),
+              ),
+            ],
           ),
         ),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _controller.currentIndex.value,
         backgroundColor: cblack,
         items: [
-          cWidget.bottomNavigationBarItem(icon: Icons.home,color: cwhite, title: ''),
+          cWidget.bottomNavigationBarItem(
+              icon: Icons.home, color: cwhite, title: ''),
           cWidget.bottomNavigationBarItem(
               icon: Icons.explore, color: cblue, title: ''),
           cWidget.bottomNavigationBarItem(
